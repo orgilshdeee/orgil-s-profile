b3a13d7b10f247da99ec6306e05d010239b0001c
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-07-06 14:10:13 +0800

    push

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", (req, res) => {
   console.log("testing");
   const temp = fs.readFileSync("./data/info.json", "utf-8");
+  // res.json(temp);
   res.send(temp);
 });
-app.listen("3001");
+app.listen("3000");
