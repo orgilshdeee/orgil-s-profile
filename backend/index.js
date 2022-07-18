0c077f8e9e1d6b547430b9e7c53d57d4aa981567
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-07-18 13:50:12 +0800

    push

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,12 @@ const app = express();
 const fs = require("fs");
 const cors = require("cors");
 const PORT = process.env.PORT;
+const names = require("./models/test.json");
+
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view options", { layout: false });
 
 app.use(
   cors({
@@ -41,6 +46,19 @@ app.get("/age", cors(), (req, res) => {
   console.log("sending age.json");
   const temp = fs.readFileSync("./models/age.json", "utf-8");
   res.send(temp);
+});
+
+app.get("/ejs", (req, res) => {
+  var data = {
+    foods: names,
+    images: {
+      cinnamon: "/foods/cinnamon.jpeg",
+    },
+  };
+  res.render("index", { data: data });
+});
+app.get("/404", (req, res) => {
+  res.render("404", { message: "oopsie!" });
 });
 
 app.listen(PORT);
